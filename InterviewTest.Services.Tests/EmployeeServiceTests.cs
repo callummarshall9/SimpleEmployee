05d879019ebaf934c89edfb61f9927b9d5f376a7
Callum Marshall <callum.marshall@corporatelinx.com>
2023-02-06 18:11:28 +0000

    PROJECT: Setup unit tests

---
+++ InterviewTest.Services.Tests/EmployeeServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using InterviewTest.Data.Interfaces;
+using InterviewTest.Model;
+using InterviewTest.Services.Interfaces;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace InterviewTest.Services.Tests
+{
+    public partial class EmployeeServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IEmployeeService employeeService;
+
+        public EmployeeServiceTests()
+        {
+            storageBrokerMock = new Mock<IStorageBroker>();
+            employeeService = new EmployeeService(storageBrokerMock.Object);
+        }
+
+        public Employee RandomEmployee()
+            => GetEmployeeFiller().Create();
+
+        public Filler<Employee> GetEmployeeFiller()
+            => new Filler<Employee>();
+    }
+}
