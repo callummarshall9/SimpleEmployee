3e5c1693e5e7448568035b982c3e46a4ccd58fff
Callum Marshall <callum.marshall@corporatelinx.com>
2023-02-06 18:11:43 +0000

    SERVICES: ShouldAddEmployeeAsync -> FAIL

---
+++ InterviewTest.Services.Tests/EmployeeServiceTests.AddAsync.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using InterviewTest.Model;
+using Moq;
+using Xunit;
+
+namespace InterviewTest.Services.Tests
+{
+    public partial class EmployeeServiceTests
+    {
+        [Fact]
+        public async void ShouldAddEmployeeAsync()
+        {
+            //given
+            Employee inputEmployee = RandomEmployee();
+            Employee expectedEmployee = inputEmployee.DeepClone();
+
+            storageBrokerMock.Setup(storageBrokerMock =>
+                storageBrokerMock.AddEmployeeAsync(inputEmployee))
+                .ReturnsAsync(expectedEmployee);
+
+            //when
+            Employee actualEmployee = await employeeService.AddEmployeeAsync(inputEmployee);
+
+            //then
+            actualEmployee.Should().BeEquivalentTo(expectedEmployee);
+
+            storageBrokerMock.Verify(storageBrokerMock =>
+                storageBrokerMock.AddEmployeeAsync(inputEmployee),
+                Times.Once());
+
+            storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
