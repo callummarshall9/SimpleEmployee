f66eaf7d261cd623a3e0447629954942d27223b8
Callum Marshall <callum.marshall@corporatelinx.com>
2023-02-07 23:12:16 +0000

    FRONTEND: Cleanup grid component

--- InterviewTest/ClientApp/src/Components/EmployeeGrid.jsx
+++ InterviewTest/ClientApp/src/Components/EmployeeGrid.jsx
@@ -1,25 +1,19 @@
-import * as React from "react";
-import "@progress/kendo-theme-default/dist/all.css";
-import * as ReactDOM from "react-dom";
 import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
+import "@progress/kendo-theme-default/dist/all.css";
+import * as React from "react";
 import { EmployeesDataLoader } from "../Data/EmployeeDataLoader";
 
 export function EmployeeGrid() {
     const [employees, setEmployees] = React.useState({
         data: [],
-        total: 77,
+        total: 0,
     });
+
     const [dataState, setDataState] = React.useState({
         take: 10,
         skip: 0,
     });
-    const dataStateChange = (e) => {
-        setDataState(e.dataState);
-    };
-    const dataReceived = (employees) => {
-        console.log(employees);
-        setEmployees(employees);
-    };
+
     return (
         <div>
             <Grid
@@ -29,13 +23,15 @@ export function EmployeeGrid() {
                 toolbar={false}
                 {...dataState}
                 data={employees}
-                onDataStateChange={dataStateChange}
+                onDataStateChange={(e) => setDataState(e.dataState)}
             >
+
                 <Column field="Name" title="Name" />
                 <Column field="Value" filter="numeric" title="Value" />
+
             </Grid>
 
-            <EmployeesDataLoader dataState={dataState} onDataReceived={dataReceived} />
+            <EmployeesDataLoader dataState={dataState} onDataReceived={(employees) => setEmployees(employees)} />
         </div>
     );
 }
